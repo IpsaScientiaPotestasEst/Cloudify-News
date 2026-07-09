6a671aabf22e08ff93004f9aa9d03fa67a310ae7
IpsaScientiaPotestasEst <msh24030@tac.school.nz>
2026-07-10 11:43:22 +1200

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 from flask_cors import CORS
 
@@ -10,19 +10,20 @@ LON = 175.3
 
 @app.route("/weather")
 def weather():
-    
+    lat = request.args.get("lat", LAT)
+    lon = request.args.get("lon", LON)
+
     url_forecast = (
         "https://api.open-meteo.com/v1/forecast"
-        f"?latitude={LAT}&longitude={LON}"
+        f"?latitude={lat}&longitude={lon}"
         "&hourly=temperature_2m,precipitation,uv_index"
         "&current_weather=true"
         "&timezone=auto"
     )
 
-    
     url_nowcast = (
         "https://api.open-meteo.com/v1/nowcast"
-        f"?latitude={LAT}&longitude={LON}"
+        f"?latitude={lat}&longitude={lon}"
         "&precipitation=true"
         "&precipitation_probability=true"
         "&precipitation_type=true"
@@ -33,7 +34,7 @@ def weather():
     raw_nowcast = requests.get(url_nowcast).json()
 
     hourly = raw_forecast.get("hourly", {})
-    nowcast = raw_nowcast
+    nowcast = raw_nowcast.get("nowcast", {})
 
     result = {
         "current": raw_forecast.get("current_weather", {}),
@@ -56,7 +57,6 @@ def weather():
 @app.route("/ping")
 def ping():
     return "ok"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
